0cc068f34a2496ce16cca2c0e83f14ed446d51fd
Yinka <asyinka@gmail.com>
2023-05-15 16:00:21 +0100

    chore: add square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import Squares from "./components/square";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-red-600">Building a Tic-Tac-Toe game</h1>
+      <Squares />
+    </div>
+  );
 }
 
 export default App;
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,5 +1,9 @@
 const Squares = () => {
-  return <div className=""></div>;
+  return (
+    <button className="border-[1px] border-solid border-black w-[34px] h-[34px] font-bold text-center text-[24px] float-left mr-[-1px] ml-[-1px] ">
+      X
+    </button>
+  );
 };
 
 export default Squares;
