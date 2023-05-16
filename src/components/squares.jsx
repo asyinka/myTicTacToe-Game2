2ce85b4d041e85157223c009b092c16e4d8635c0
Yinka <asyinka@gmail.com>
2023-05-16 16:36:41 +0100

    chore: set value prop fro each square

--- src/components/squares.jsx
+++ src/components/squares.jsx
@@ -1,6 +1,6 @@
 const Squares = ({ value }) => {
   return (
-    <button className=" w-[34px] h-[34px] float-left border-[1px] border-solid border-blue-950 p-[2px] ">
+    <button className=" w-[34px] h-[34px] float-left border-[1px] border-solid border-blue-950 p-[2px] text-lg font-semibold text-green-900 ">
       {value}
     </button>
   );
--- src/services/Board.jsx
+++ src/services/Board.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import Squares from "../components/Squares";
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // const [value, setValue] = useState(null)
+
+  function handleClick() {}
   return (
     <div className="w-fit grid grid-cols-3 mt-3 bg-amber-200">
-      <Squares />
-      <Squares />
-      <Squares />
-      <Squares />
-      <Squares />
-      <Squares />
-      <Squares />
-      <Squares />
-      <Squares />
+      <Squares value={squares[0]} />
+      <Squares value={squares[1]} />
+      <Squares value={squares[2]} />
+      <Squares value={squares[3]} />
+      <Squares value={squares[4]} />
+      <Squares value={squares[5]} />
+      <Squares value={squares[6]} />
+      <Squares value={squares[7]} />
+      <Squares value={squares[8]} />
     </div>
   );
 };
