eacb1abd91a8f090c3b651c9697581f51b300cd0
Yinka <asyinka@gmail.com>
2023-05-16 13:07:03 +0100

    process: game can now be marked X

--- src/services/Board.jsx
+++ src/services/Board.jsx
@@ -2,22 +2,31 @@ import { useState } from "react";
 import Squares from "../components/square";
 
 const Board = () => {
-  function handleClick() {}
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    //to avoid mutating our array we call .slice() to create another array
+    const nextSquare = squares.slice();
+    //change the value of item at index 'i' from 'null' to 'X' when clicked
+    nextSquare[i] = "X";
+    //update the state of squares, since the values changed. This also re-renders the component
+    setSquares(nextSquare);
+  }
 
   return (
     <div className="grid grid-cols-3 w-fit">
-      <Squares value={squares[0]} onSquareClick={handleClick} />
-      <Squares value={squares[1]} />
-      <Squares value={squares[2]} />
+      <Squares value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Squares value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Squares value={squares[2]} onSquareClick={() => handleClick(2)} />
 
-      <Squares value={squares[3]} />
-      <Squares value={squares[4]} />
-      <Squares value={squares[5]} />
+      <Squares value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Squares value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Squares value={squares[5]} onSquareClick={() => handleClick(5)} />
 
-      <Squares value={squares[6]} />
-      <Squares value={squares[7]} />
-      <Squares value={squares[8]} />
+      <Squares value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Squares value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Squares value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   );
 };
