c271240a164a0f555f727b2ee9d5b041b890e0e9
Yinka <asyinka@gmail.com>
2023-05-17 15:19:24 +0100

    game can now go back to previous moves

--- src/services/Game.jsx
+++ src/services/Game.jsx
@@ -5,19 +5,58 @@ const Game = () => {
   //tracks all the moves
   const [history, setHistory] = useState([Array(9).fill(null)]);
   //toggles xIsNext status
-  const [xIsNext, setXIsNext] = useState(true);
+  const xIsNext = nextMove % 2 === 0;
+  //monitors which step the user is on
+  const [currentMove, setCurrentMove] = useState(0);
   //replaces square; the current move
-  const currentSquare = history[history.length - 1];
-  //
+  const currentSquare = history[currentMove];
+
+  //this jumps to the move the user clicks on
+  function jumpTo(nextMove) {
+    //updates the user's move according to its move
+    setCurrentMove(nextMove);
+    //X will be true on every move that is even
+    // setXIsNext(nextMove % 2 === 0);
+  }
+
   function handlePlay(currentSquare) {
-    setHistory([...history, currentSquare]);
+    //this goes back to the clicked square, removing unwanted squares
+    const nextHistory = [...history.slice(0, currentMove + 1), currentSquare];
+    //ensures the squares clicked tallies with the jumpTo function
+    setHistory(nextHistory);
+    //updates the users move
+    setCurrentMove(nextHistory.length - 1);
 
     setXIsNext(!xIsNext);
   }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = `Game at move # ${move}`;
+    } else {
+      description = `Game start`;
+    }
+
+    return (
+      <li key={move}>
+        <button
+          onClick={() => jumpTo(move)}
+          className="m-2 bg-emerald-600 rounded-[10px] p-2 "
+        >
+          {description}
+        </button>
+      </li>
+    );
+  });
+
   return (
     <div>
       <Board squares={currentSquare} xIsNext={xIsNext} onPlay={handlePlay} />
-      <div></div>
+      <div>
+        <ul className="mt-2 bg-gray-400">{moves}</ul>
+      </div>
     </div>
   );
 };
